54c9375404b1751b71c15f47fdfd0e384357a0f2
duwart <60189792+duwart@users.noreply.github.com>
2021-08-02 18:59:05 -0300

    Tela de autenticação
    Verificação

--- src/TelaLogin/Login.js
+++ src/TelaLogin/Login.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Alert } from "react-native";
-import { TouchableOpacityBase } from "react-native";
-import { Platform } from "react-native";
+import React, {useState, useEffect} from 'react';
+import {Alert} from 'react-native';
+import {TouchableOpacityBase} from 'react-native';
+import {Platform} from 'react-native';
 import {
   View,
   Image,
@@ -10,42 +10,49 @@ import {
   TextInput,
   StyleSheet,
   Text,
-} from "react-native";
+} from 'react-native';
 
-export default ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const [errorLogin, setErrorLogin] = useState("");
-  const [variavel, setvariavel] = useState("");
+export default ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [errorLogin, setErrorLogin] = useState('');
+  const [variavel, setvariavel] = useState('');
 
   useEffect(() => {}, []);
 
   function login(senha, email) {
-    console.log("semha: " + senha + " email: " + email);
+    console.log('semha: ' + senha + ' email: ' + email);
 
-    fetch(`http://192.168.1.103:5000/store/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(senha)}`, {
-      method: "GET",
-    })
-      .then((response) => response.text())
-      .then((texto) => {console.log(texto)})
-      //.then((json) => setvariavel(json))
-      //.catch((error) => console.error(error))
-      //.finally(() => setLoading(false));
-    //console.log("saida:" + variavel);
+    fetch(
+      `http://192.168.1.103:5000/store/login?email=${encodeURIComponent(
+        email,
+      )}&password=${encodeURIComponent(senha)}`,
+      {
+        method: 'GET',
+      },
+    )
+      .then(response => response.text())
+      .then(texto => {
+        setvariavel(texto);
+      });
+    if (variavel != 'Non Authorized') {
+      navigation.navigate('ProdList', variavel[0]);
+    } else {
+      Alert.alert('Error');
+    }
   }
 
   return (
     <KeyboardAvoidingView
       style={styles.background}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-    >
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
       <View style={styles.containerLogo}>
         <Image
           style={{
             width: 170,
             height: 155,
           }}
-          source={require("../../assets/Op_1.png")}
+          source={require('../../assets/Op_1.png')}
         />
       </View>
       <View style={styles.container}>
@@ -53,7 +60,7 @@ export default ({ navigation }) => {
           style={styles.input}
           placeholder="Email"
           type="text"
-          onChangeText={(text) => {
+          onChangeText={text => {
             setEmail(text);
           }}
           value={email}
@@ -66,37 +73,35 @@ export default ({ navigation }) => {
           placeholder="Senha"
           autoCorrect={false}
           type="text"
-          onChangeText={(text) => {
+          onChangeText={text => {
             setSenha(text);
           }}
           value={senha}
         />
         {errorLogin === true ? (
           <View>
-            <Text style={{ color: "red" }}> E-mail ou senha inválidos</Text>
+            <Text style={{color: 'red'}}> E-mail ou senha inválidos</Text>
           </View>
         ) : (
           <View></View>
         )}
-        {email === "" || senha === "" ? (
+        {email === '' || senha === '' ? (
           <TouchableOpacity style={styles.btnsubmit} onPress={() => {}}>
             <Text style={styles.textsubmit}>Acessar</Text>
           </TouchableOpacity>
         ) : (
           <TouchableOpacity
             style={styles.btnsubmit}
-            onPress={() => login(senha, email)}
-          >
+            onPress={() => login(senha, email)}>
             <Text style={styles.textsubmit}>Acessar</Text>
           </TouchableOpacity>
         )}
 
         <TouchableOpacity
           onPress={() => {
-            navigation.navigate("CadastroLogin");
+            navigation.navigate('CadastroLogin');
           }}
-          style={styles.btnregister}
-        >
+          style={styles.btnregister}>
           <Text style={styles.textregister}>Cadastrar</Text>
         </TouchableOpacity>
       </View>
@@ -107,25 +112,25 @@ export default ({ navigation }) => {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#10d177",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#10d177',
   },
   containerLogo: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   container: {
-    alignItems: "center",
+    alignItems: 'center',
     flex: 1,
-    width: "90%",
-    justifyContent: "center",
+    width: '90%',
+    justifyContent: 'center',
   },
   input: {
-    backgroundColor: "#fff",
-    width: "90%",
+    backgroundColor: '#fff',
+    width: '90%',
     marginBottom: 15,
-    color: "#222",
+    color: '#222',
     fontSize: 17,
     borderRadius: 7,
     padding: 10,
@@ -133,31 +138,31 @@ const styles = StyleSheet.create({
   btnsubmit: {
     width: 200,
     height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "darkorange",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'darkorange',
     borderRadius: 50,
     marginTop: 10,
   },
   textsubmit: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 18,
   },
   btnregister: {
     marginTop: 10,
   },
   textregister: {
-    color: "#fff",
+    color: '#fff',
   },
   contentAlert: {
     marginTop: 20,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   warningAlert: {
     paddingLeft: 10,
-    color: "#bdbdbd",
+    color: '#bdbdbd',
     fontSize: 16,
   },
 });
